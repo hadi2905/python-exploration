66e317d364aa65478df51379de474f0528f2ae46
hadi2905 <harrdie2905@gmail.com>
2018-12-29 19:41:53 +0100

    Neue Dateien ergaenzt

---
+++ logging.py
@@ -0,0 +1,21 @@
+import logging
+import mit_uhrzeiten_rechnen as mur
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+fH = logging.FileHandler("test_logger.log")
+fH.setLevel(logging.DEBUG)
+
+cH = logging.StreamHandler()
+cH.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fH.setFormatter(formatter)
+cH.setFormatter(formatter)
+
+logger.addHandler(fH)
+logger.addHandler(cH)
+
+logger.info("erster Aufruf")
+mur.do_something()
---
+++ mit_uhrzeiten_rechnen.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import datetime as dt
+import logging
+
+print("__name__=",__name__)
+
+def do_something():
+    rtc_time = dt.time(23,20,0)
+    recentAlarm = dt.datetime.combine(dt.datetime.today(), rtc_time)
+    nextAlarm = recentAlarm + dt.timedelta(minutes=15)
+    logging.info(nextAlarm)
+    nextAlarm = recentAlarm + dt.timedelta(hours=1)
+    logging.info(nextAlarm)
+    nextAlarm = recentAlarm + dt.timedelta(hours=2, minutes=30)
+    logging.info(nextAlarm)
+    print("Done")
