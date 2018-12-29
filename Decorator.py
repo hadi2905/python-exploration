f997b4858ccfc55839fa299b006aeb8414859af0
hadi2905 <harrdie2905@gmail.com>
2018-12-29 19:26:30 +0100

    Method multiplikation added

--- Decorator.py
+++ Decorator.py
@@ -27,6 +27,14 @@ def zufall(wert, seed=0):       # nimmt einen Positions- und einen benannten Par
     print('so ein Zufall')
     return wert*random.random()
 
+@mydeco
+def multiplikation(*args):
+    wert = 1
+    for w in args:
+        wert *= w
+    return wert
+
 if __name__=='__main__':
     print(addition(5,16))
     print(zufall(99, seed=50))
+    print(multiplikation(10,3,6,5))
