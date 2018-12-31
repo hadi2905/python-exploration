7f6600ec3adb0ed39e534a5aa0809f7ef50f3586
hadi2905 <harrdie2905@gmail.com>
2018-12-31 16:48:36 +0100

    Exception handling in socket files added

--- socket_client.py
+++ socket_client.py
@@ -5,14 +5,17 @@ Implementiert einen einfachen Socket-Client
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('127.0.0.214', 2400))
 
-while True:
-    msg = str(input('Eingabe?'))
-    msg = msg.encode()
-    s.send(msg)
+try:
+    while True:
+        msg = str(input('Eingabe?'))
+        msg = msg.encode()
+        s.send(msg)
 
-    msg = s.recv(1024)
-    msg = msg.decode()
-    print('Server antwortet: ' + msg)
+        msg = s.recv(1024)
+        msg = msg.decode()
+        print('Server antwortet: ' + msg)
+except KeyboardInterrupt:
+    print('Abgebrochen')
 
 s.close()
 
--- socket_server.py
+++ socket_server.py
@@ -4,23 +4,23 @@ Implementiert einen einfachen Socket-Server
 """
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-serversocket.bind(('192.168.0.10', 54000))
+serversocket.bind(('127.0.0.214', 2400))
 serversocket.listen(0)
+try:
+    (clientsocket, adress) = serversocket.accept()
+    print('client connected')
 
-(clientsocket, adress) = serversocket.accept()
-print('client connected')
+    while True:
+        msg= clientsocket.recv(1024)
+        msg = msg.decode()
+        print(msg)
 
-
-while True:
-    msg= clientsocket.recv(1024)
-    msg = msg.decode()
-    print(msg)
-
-    ans = "Server hat folgende Anfrage bekommen: " + msg
-    print(ans)
-    ans = ans.encode()
-    clientsocket.send(ans)
+        ans = "Server hat folgende Anfrage bekommen: " + msg
+        print(ans)
+        ans = ans.encode()
+        clientsocket.send(ans)
+except KeyboardInterrupt:
+    print('Abgebrochen')
 
 clientsocket.close()
 serversocket.close()
-/usr/bin
